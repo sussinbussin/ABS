7047a4d59b63035c604a70b542a2ad33ca128ad7
jz <t.jingzhi1@gmail.com>
2023-03-16 13:24:01 +0800

    added inventory to user store

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -5,10 +5,12 @@ import { useStorage } from '@vueuse/core'
 export const useUserStore = defineStore('user', () => {
   const user = ref(useStorage('user', {}))
   const cca = ref(useStorage('cca', {}))
+  const inventory = ref(useStorage('inventory', []))
 
   const clear = () => {
     user.value = {}
     cca.value = {}
+    inventory.value = []
   }
-  return { user, cca, clear }
+  return { user, cca, inventory, clear }
 })
