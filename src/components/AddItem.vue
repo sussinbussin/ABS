42594b2aa7697401335fb62ca9a088a9d2ab7465
yltan <yaolongt@gmail.com>
2023-03-20 13:27:30 +0800

    error message for additem

--- src/components/AddItem.vue
+++ src/components/AddItem.vue
@@ -5,7 +5,6 @@ import { useUserStore } from '~/stores/user'
 import { pbSymbol } from '~/symbols/injectionSymbols'
 
 import { useToast } from 'vue-toast-notification'
-import { error } from 'console'
 
 const emit = defineEmits(['done'])
 const pb = inject(pbSymbol)
@@ -15,51 +14,71 @@ const toast = useToast();
 //TODO: add based on fields
 const name = ref("")
 const qty = ref(0)
+const nameError = ref(true)
+const qtyError = ref(true)
+
+const nameValid = computed(() => {
+  if (name.value) {
+    nameError.value = false
+    return ""
+  } else {
+    nameError.value = true
+    return "Name cannot be empty!"
+  }
+})
+
+const qtyValid = computed(() => {
+  if (qty.value > 0) {
+    qtyError.value = false
+    return ""
+  } else {
+    qtyError.value = true
+    return "Quantity must be more than 0!"
+  }
+})
 
 const submit = async () => {
-  try {
-    if (name.value == "") {
-      throw new Error("Name should not be empty")
-    } else if (qty.value <= 0) {
-      throw new Error("Value should not be less than or equal 0")
+  if (!nameError.value && !qtyError.value) {
+    try {
+      const res = await pb?.collection('inventory').create({
+        name: name.value,
+        qty: qty.value,
+        cca: userStore.cca.id
+      })
+      toast.open({
+        type: "success",
+        message: "Item Added!",
+        position: "bottom"
+      })
+
+    } catch (err) {
+      toast.open({
+        type: "error",
+        message: `${err}`,
+        position: "bottom"
+      })
+
     }
 
-    const res = await pb?.collection('inventory').create({
-      name: name.value,
-      qty: qty.value,
-      cca: userStore.cca.id
-    })
-    toast.open({
-      type: "success",
-      message: "Item Added!",
-      position: "bottom"
+    const inventoryRes = await pb?.collection('inventory').getFullList(500, {
+      filter: `cca.id="${userStore.cca.id}"`,
     })
 
-  } catch (err) {
-    toast.open({
-      type: "error",
-      message: `${err}`,
-      position: "bottom"
-    })
+    userStore.inventory = inventoryRes
 
+    name.value = ""
+    qty.value = 0
+
+    emit("done")
   }
-
-  const inventoryRes = await pb?.collection('inventory').getFullList(500, {
-    filter: `cca.id="${userStore.cca.id}"`,
-  })
-
-  userStore.inventory = inventoryRes
-
-  emit("done")
-
 }
 </script>
 
 <template>
   <ACard title="Add Item" class="px-5 pb-6">
     <form class="grid-row place-items-stretch" @submit.prevent="submit">
-      <AInput v-model="name" placeholder="name" type="text" class-text-sm />
-      <AInput v-model="qty" placeholder="qty" type="text" class-text-sm />
+      <AInput v-model="name" :error="nameValid" placeholder="name" type="text" class-text-sm />
+      <AInput v-model="qty" :error="qtyValid" placeholder="qty" type="number" class-text-sm />
       <ABtn>Submit</ABtn>
     </form>
   </ACard>
