38928c8645a8469fb4771eab9929409c888d6923
yltan <yaolongt@gmail.com>
2023-03-21 21:56:02 +0800

    card view done
    - toggle list and card view done

--- src/pages/Inventory.vue
+++ src/pages/Inventory.vue
@@ -2,6 +2,12 @@
 import { ref } from 'vue'
 
 const showDialog = ref(false)
+
+const isListView = ref(true)
+
+const toggleView = () => {
+  isListView.value = !isListView.value
+}
 </script>
 
 <template>
@@ -9,13 +15,24 @@ const showDialog = ref(false)
     <h1 class="font-bold text-xl p-5 text-left">
       My Inventory
     </h1>
+
+    <div class="flex w-2/3">
+      <ABtn class="m-2 w-1/3" @click="toggleView()">
+        <div v-if="isListView">List View</div>
+        <div v-else>Card View</div>
+      </ABtn>
+    </div>
+
     <Suspense class="flex-grow">
-      <InventoryTable />
+      <InventoryTable v-if="isListView"/>
+      <InventoryCard v-else />
     </Suspense>
-    <div class="flex flex-row p-3 mb-auto">
+
+    <div class="flex flex-row p-3 mb-auto" >
       <ABtn icon='i-bx-plus' class="ml-auto mr-1" @click="showDialog = true">Add Item</ABtn>
     </div>
-    <ADialog v-model="showDialog" class="w-[900px]">
+
+    <ADialog v-model="showDialog">
       <AddItem :show-dialog="showDialog" @done="showDialog = false" />
     </ADialog>
   </div>
