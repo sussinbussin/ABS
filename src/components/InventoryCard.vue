d14d385be8ade82379783e500401d0cabdf52a27
yltan <yaolongt@gmail.com>
2023-03-24 02:40:07 +0800

    show multiple cards

--- src/components/InventoryCard.vue
+++ src/components/InventoryCard.vue
@@ -19,21 +19,22 @@ const inventoryGroup = inventory.value.reduce((acc, item) => {
 }, {})
 
 // open multiple cards
-// const expanded = ref(new Array(inventory.length).fill(false))
+const expanded = ref(Object.fromEntries(Object.keys(inventoryGroup).map(key => [key, false])))
+console.log(expanded)
 
 // open one card only
-const expanded = ref(-1)
+// const expanded = ref(-1)
 
-const toggleExpand = (index: number) => {
+const toggleExpand = (index: string) => {
     // open multiple cards
-    // expanded.value[index] = !expanded.value[index]
-
+    expanded.value[index] = !expanded.value[index]
+    
     // open one card only
-    if (index == expanded.value) {
-        expanded.value = -1
-    } else {
-        expanded.value = index
-    }
+    // if (index == expanded.value) {
+    //     expanded.value = -1
+    // } else {
+    //     expanded.value = index
+    // }
 }
 
 const cardsRef = ref<HTMLElement>()
@@ -61,13 +62,13 @@ const getImageUrl = (items: Array<Record>) => {
         <div class="grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5" ref="cardsRef"
             v-motion-slide-right>
             <div class="flex flex-col h-auto border border-transparent rounded-lg bg-gray-8 mx-2 my-5 hover:bg-gray-7 hover:scale-103% transition-transform duration-300"
-                v-for="(items, index) in inventoryGroup" :key="items.id" :style="{
+                v-for="(items, index) in inventoryGroup" :key="index" :style="{
                     // open multiple cards change: expanded => expanded[index]
                     // the following 3 styles allows overlapping
                     // good for opening 1 card at a time
-                    position: expanded == index ? 'relative' : 'static',
-                    zIndex: expanded == index ? '1' : '0',
-                    marginBottom: expanded == index ? '-145px' : '0'
+                    // position: expanded == index ? 'relative' : 'static',
+                    // zIndex: expanded == index ? '1' : '0',
+                    // marginBottom: expanded == index ? '-145px' : '0'
                 }">
                 <div class="border border-gray-7 rounded-lg h-150px mb-1 flex justify-center ">
                     <img :src="getImageUrl(items) ? getImageUrl(items) : imagePlaceHolder" alt="Image"
@@ -84,7 +85,7 @@ const getImageUrl = (items: Array<Record>) => {
                     </div>
                 </div>
 
-                <div v-show="expanded == index" class="">
+                <div v-show="expanded[index]" class="">
                     <div>PLACEHOLDER</div>
                     <div>PLACEHOLDER</div>
                     <div>PLACEHOLDER</div>
@@ -95,9 +96,8 @@ const getImageUrl = (items: Array<Record>) => {
 
 
                 <hr class="m-2">
-
                 <ABtn variant="text" @click="toggleExpand(index)" class="flex justify-end w-full">
-                    <div v-if="!(expanded == index)">Show</div>
+                    <div v-if="!expanded[index]">Show</div>
                     <div v-else>Hide</div>
                 </ABtn>
             </div>
