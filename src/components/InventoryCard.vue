8dd56b6ba8df2d4c2424ebe18e682e5c01da057d
yltan <yaolongt@gmail.com>
2023-03-26 00:46:22 +0800

    removed expand, changed to hover change page
    transition added

--- src/components/InventoryCard.vue
+++ src/components/InventoryCard.vue
@@ -42,10 +42,10 @@ const getImageUrl = (items: Array<Record>) => {
     const item = ref()
     // console.log(typeof items)
     items.forEach((e: Object) => {
-         if (e.image !== '') {
+        if (e.image !== '') {
             item.value = e
             return false
-         }
+        }
     })
 
     if (item.value !== undefined) {
@@ -54,6 +54,19 @@ const getImageUrl = (items: Array<Record>) => {
         return null
     }
 }
+
+const normalDisplay = ref('')
+
+const assetsDisplay = (index: string) => {
+    console.log(normalDisplay.value)
+    console.log(index);
+    if (index != '') {
+        normalDisplay.value = index
+    } else {
+        normalDisplay.value = ''
+    }
+}
+
 </script>
 
 <template>
@@ -61,47 +74,50 @@ const getImageUrl = (items: Array<Record>) => {
         <div class="grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5" ref="cardsRef"
             v-motion-slide-right>
             <div class="flex flex-col border border-transparent rounded-lg bg-gray-8 mx-2 my-5 hover:bg-gray-7 hover:scale-103% transition-transform duration-300"
-                v-for="(items, index) in inventoryGroup" :key="items.id" :style="{
-                    // open multiple cards change: expanded => expanded[index]
-                    // the following 3 styles allows overlapping
-                    // good for opening 1 card at a time
-                    position: expanded == index ? 'relative' : 'static',
-                    zIndex: expanded == index ? '1' : '0',
-                    marginBottom: expanded == index ? '-145px' : '0'
-                }">
-                <div class="border border-gray-7 rounded-lg h-150px mb-1 flex justify-center ">
-                    <img :src="getImageUrl(items) ? getImageUrl(items) : imagePlaceHolder" alt="Image"
-                        class="max-w-100% max-h-100% object-contain" />
-                </div>
-
-
-                <div class="mx-2 text-justify">
-                    <div class="truncate font-bold text-5">
-                        {{ index }}
-                    </div>
-                    <div class="truncate text-3.5 text-gray-4">
-                        Qty: {{ items.length }}
-                    </div>
-                </div>
-
-                <div v-show="expanded == index" class="h-36 overflow-y-scroll">
-                    <div v-for="e in items" :key="e" class="flex flex-row justify-between mx-2 text-3">
-                        <div>
-                            {{ e.asset_tag }}
+                v-for="(items, index) in inventoryGroup" :key="index" @mouseenter="assetsDisplay(index)"
+                @mouseleave="assetsDisplay('')">
+                <div class="h-210px relative overflow-hidden">
+                    <div :class="normalDisplay == index ? 'translate-y-0' : 'translate-y-full'"
+                        class="h-full absolute top-0 left-0 w-full transition-all duration-300">
+                        <div class="h-95% my-1 overflow-y-scroll">
+                            <div v-for="e in items" :key="e" class="flex flex-row justify-between mx-2 text-3">
+                                <div>
+                                    {{ e.asset_tag }}
+                                </div>
+                                <div>
+                                    {{ e.asset_condition }}
+                                </div>
+                            </div>
                         </div>
-                        <div>
-                            {{ e.asset_condition }}
+
+                    </div>
+
+                    <div :class="normalDisplay !== index ? 'translate-y-0' : '-translate-y-full'"
+                        class="h-100% absolute top-0 left-0 w-full transition-all duration-300">
+                        <div class="border border-gray-7 rounded-lg h-70% mb-1 flex justify-center ">
+                            <img :src="getImageUrl(items) ? getImageUrl(items) : imagePlaceHolder" alt="Image"
+                                class="max-w-100% max-h-100% object-contain" />
+                        </div>
+
+
+                        <div class="mx-2 text-justify">
+                            <div class="truncate font-bold text-5">
+                                {{ index }}
+                            </div>
+                            <div class="truncate text-3.5 text-gray-4">
+                                Qty: {{ items.length }}
+                            </div>
                         </div>
                     </div>
                 </div>
 
 
-                <hr class="m-2">
+                <!-- <hr class="m-2"> -->
 
-                <ABtn variant="text" @click="toggleExpand(index)" class="flex justify-end w-full">
-                    <div v-if="!(expanded == index)">Show</div>
-                    <div v-else>Hide</div>
-                </ABtn>
+                <!-- <ABtn variant="text" @click="toggleExpand(index)" class="flex justify-end w-full">
+                                <div v-if="!(expanded == index)">Show</div>
+                                <div v-else>Hide</div>
+                            </ABtn> -->
             </div>
         </div>
     </div>
