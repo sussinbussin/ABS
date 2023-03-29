ac7c83b09f3f9320532028c2c837f56b5e177f09
yltan <yaolongt@gmail.com>
2023-03-30 02:07:00 +0800

    dialog open, with editable fields
    - will do confirm update tmr

--- src/components/CardItem.vue
+++ src/components/CardItem.vue
@@ -1,33 +1,44 @@
 <script setup lang="ts">
-import { DialogPanel, DialogTitle, DialogDescription } from '@headlessui/vue';
+import { DialogPanel, DialogDescription } from "@headlessui/vue"
+import { ref } from 'vue'
 
-const props = defineProps<{ title: string, description: string }>();
+const props = defineProps<{ itemName: string, items: Object }>();
+const selectedConditions = ref<[]>([])
+const selectedLoanStatus = ref<[]>([])
 
-const isOpen = ref(true)
+const conditions = ['Working', 'Damaged', 'Disposed']
+const loanStatus = ['Available for loan', 'Not available for loan', 'Out on loan', 'Under maintenance', 'To be disposed']
 
-  function setIsOpen(value) {
-    isOpen.value = value
-  }
+const updateCondition = (index: string, condition: string) => {
+  console.log("Selected condition: ", condition)
+  selectedConditions.value[parseInt(index)] = condition // update the selected condition value for the corresponding record index
+}
+
+const updateLoanStatus = (index: string, loanStatus: string) => {
+  console.log("Selected condition: ", index)
+  console.log(selectedLoanStatus.value)
+  selectedLoanStatus.value[parseInt(index)] = loanStatus // update the selected condition value for the corresponding record index
+}
+
 </script>
 
 <template>
-  <DialogPanel class="bg-white">
-    <div>{{ props.title }}</div>
-    <div>{{ props.description }}</div>
-  <!-- </DialogPanel> -->
-
-  <!-- <DialogPanel> -->
-      <DialogTitle>Deactivate account</DialogTitle>
-      <DialogDescription>
-        This will permanently deactivate your account
-      </DialogDescription>
-
-      <p>
-        Are you sure you want to deactivate your account? All of your data will be
-        permanently removed. This action cannot be undone.
-      </p>
-
-      <button @click="setIsOpen(false)">Deactivate</button>
-      <button @click="setIsOpen(false)">Cancel</button>
-    </DialogPanel>
+  <div>
+    <div v-for="(e, index) in props.items" :key="e.asset_tag">
+      <div class="grid grid-cols-3 justify-between">
+        <div class="flex items-center border border-gray-7 rounded-lg m-2 p-2 bg-gray-8 opacity-50">{{ e.asset_tag }}
+        </div>
+        <select @change="updateCondition(index, $event.target.value)" class="bg-black flex items-center border border-gray-8 rounded-lg m-2 p-2">
+          <option value="" disabled selected hidden>{{ e.asset_condition || "Available for loan"}}</option>
+          <option v-for="option in conditions" :key="option" :value="option">{{ option }}</option>
+        </select>
+        <select @change="updateLoanStatus(index, $event.target.value)" class="bg-black flex items-center border border-gray-8 rounded-lg m-2 p-2">
+          <option value="" disabled selected hidden>{{ e.loan_status }}</option>
+          <option v-for="option in loanStatus" :key="option" :value="option">{{ option }}</option>
+        </select>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style scoped></style>
--- src/components/InventoryCard.vue
+++ src/components/InventoryCard.vue
@@ -6,8 +6,9 @@ import { useUserStore } from '~/stores/user'
 import { pbSymbol } from '~/symbols/injectionSymbols'
 import { Dialog } from '@headlessui/vue'
 
+
 interface CategoryMap {
-    [key: string]: { assets: Array<string>; color: string }
+  [key: string]: { assets: Array<string>; color: string }
 }
 
 const userStore = useUserStore()
@@ -17,7 +18,10 @@ const cardsRef = ref<HTMLElement>()
 const normalDisplay = ref('')
 const search = ref('')
 const cardItem = ref(false)
-const childDetails = ref({title: "test", description: "test"});
+const childDetails = ref<{ itemName: string, items: Object }>({
+  itemName: '',
+  items: []
+});
 const categoryColors = ['red', 'green', 'blue', 'orange', 'yellow']
 
 const inventory = computed(() => userStore.inventory)
@@ -26,150 +30,162 @@ const inventory = computed(() => userStore.inventory)
 
 // Group assets into same name
 const inventoryGroup = inventory.value.reduce((acc, item) => {
-    if (!acc[item.asset_name.trim()])
-        acc[item.asset_name.trim()] = []
+  if (!acc[item.asset_name.trim()])
+    acc[item.asset_name.trim()] = []
 
-    acc[item.asset_name.trim()].push(item)
-    return acc
+  acc[item.asset_name.trim()].push(item)
+  return acc
 }, {})
 
 // Search function
 const displayedCards = computed(() => {
-    if (!search.value || !search.value.trim()) {
-        return inventoryGroup
-    }
-    else {
-        const filtered = {}
-        Object.keys(inventoryGroup).forEach((key) => {
-            if (key.toLowerCase().includes(search.value.toLowerCase()))
-                filtered[key] = inventoryGroup[key]
-        })
-        return filtered
-    }
+  if (!search.value || !search.value.trim()) {
+    return inventoryGroup
+  }
+  else {
+    const filtered = {}
+    Object.keys(inventoryGroup).forEach((key) => {
+      if (key.toLowerCase().includes(search.value.toLowerCase()))
+        filtered[key] = inventoryGroup[key]
+    })
+    return filtered
+  }
 })
 
 // Group assets into each category with color
 const category: CategoryMap = Object.keys(inventoryGroup).reduce((acc, key, index) => {
-    const itemCategory = inventoryGroup[key][0].category
-    if (!acc[itemCategory])
-        acc[itemCategory] = { assets: [], color: '' }
+  const itemCategory = inventoryGroup[key][0].category
+  if (!acc[itemCategory])
+    acc[itemCategory] = { assets: [], color: '' }
 
-    acc[itemCategory].assets.push(key)
-    // TODO: assign color to each category for AChip correctly
-    acc[itemCategory].color = categoryColors[index % categoryColors.length]
-    return acc
+  acc[itemCategory].assets.push(key)
+  // TODO: assign color to each category for AChip correctly
+  acc[itemCategory].color = categoryColors[index % categoryColors.length]
+  return acc
 }, {})
 
 // Get image for each group
 const getImageUrl = (items: Array<Record>) => {
-    const item = ref()
-    // console.log(typeof items)
-    items.forEach((e: Object) => {
-        if (e.image !== '') {
-            item.value = e
-            return false
-        }
-    })
+  const item = ref()
+  // console.log(typeof items)
+  items.forEach((e: Object) => {
+    if (e.image !== '') {
+      item.value = e
+      return false
+    }
+  })
 
-    if (item.value !== undefined)
-        return `${pb?.collection('assets').client.baseUrl}/api/files/assets/${item.value.id}/${item.value.image}`
-    else
-        return null
+  if (item.value !== undefined)
+    return `${pb?.collection('assets').client.baseUrl}/api/files/assets/${item.value.id}/${item.value.image}`
+  else
+    return null
 }
 
 // Switch display card screen on hover
 const assetsDisplay = (index: string) => {
-    if (index !== '')
-        normalDisplay.value = index
-    else
-        normalDisplay.value = ''
+  if (index !== '')
+    normalDisplay.value = index
+  else
+    normalDisplay.value = ''
 }
 
-const handleClick = (item: any) => {
-    cardItem.value = true;
-    // set child details here
-    childDetails.value.title = "changed title";
-    childDetails.value.description = "changed description";
+const handleClick = (itemName: string, item: Object) => {
+  cardItem.value = true;
+  // set child details here
+  childDetails.value.itemName = itemName
+  childDetails.value.items = item
 }
 
-const setCardOpen = () => {
-    cardItem.value = false
+const setCardClose = () => {
+  cardItem.value = false
 }
 </script>
 
 <template>
-    <div>
-        <AInput v-model="search" placeholder="Search Name" type="text" class="text-sm mx-2" />
-        <div class="w-full max-h-600px overflow-y-scroll overflow-x-hidden">
-            <div ref="cardsRef" v-motion-slide-right
-                class="grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
-                <div v-for="(items, index) in displayedCards" :key="index"
-                    class="flex flex-col border border-transparent rounded-lg bg-#2c2c2c mx-2 my-5 hover:bg-#5f5f5f hover:scale-103% transition-transform duration-300"
-                    @click="handleClick(items)" @mouseenter="assetsDisplay(index)" @mouseleave="assetsDisplay('')" >
-                    <div class="h-270px relative overflow-hidden">
-                        <div :class="normalDisplay === index ? 'translate-y-0' : 'translate-y-full'"
-                            class="h-full absolute top-0 left-0 w-full transition-all duration-300">
-                            <div class="h-95% my-1 overflow-y-scroll ">
-                                <div v-for="e in items" :key="e" class="flex flex-row justify-between mx-2 text-3">
-                                    <div>
-                                        {{ e.asset_tag }}
-                                    </div>
-                                    <div>
-                                        {{ e.asset_condition }}
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div :class="normalDisplay !== index ? 'translate-y-0' : '-translate-y-full'"
-                            class="flex flex-col justify-evenly h-full pb-2 absolute top-0 left-0 w-full transition-all duration-300">
-                            <div class="border border-gray-7 rounded-lg h-70% mb-1 flex justify-center ">
-                                <img :src="getImageUrl(items) ? getImageUrl(items) : imagePlaceHolder" alt="Image"
-                                    class="max-w-100% max-h-100% object-contain rounded-lg">
-                            </div>
-
-                            <div class="mx-2 text-justify">
-                                <div class="truncate font-bold text-5">
-                                    {{ index }}
-                                </div>
-                                <div class="truncate text-3.5 text-gray-4">
-                                    Qty: {{ items.length }}
-                                </div>
-                                <div class="truncate text-3.5">
-                                    <AChip :color="category[items[0].category].color">
-                                        {{ items[0].category }}
-                                    </AChip>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  <div class="z-1">
+    <Dialog :open="cardItem" @close="setCardClose" class="fixed h-full w-full top-25% left-30% z-2" >
+      <div class="fixed inset-0 bg-black/50"/>
+      <div class="absolute w-40% h-50% rounded-lg bg-black p-2 pb-7">
+        <div class="text-white font-bold text-6">{{ childDetails.itemName }}</div>
+        <div class="w-full h-80% overflow-y-scroll my-2">
+          <CardItem :itemName="childDetails.itemName" :items="childDetails.items"/>
         </div>
-        <Dialog :open="cardItem" @close="setCardOpen" class="z-50">
-                        <CardItem :title="childDetails.title" :description="childDetails.description" />
-                        <button @click="setCardOpen">Cancel</button>
-                    </Dialog>
+        <div class="flex flex-row justify-between">
+          <ABtn @click="setCardClose" class="w-48%">Cancel</ABtn>
+          <ABtn @click="setCardClose" class="w-48%" color="warning" variant="light">Confirm</ABtn>
+        </div>
+       
+      </div>
+    </Dialog>
+
+    <AInput v-model="search" placeholder="Search Name" type="text" class="text-sm mx-2" />
+    <div class="w-full max-h-600px overflow-y-scroll overflow-x-hidden">
+      <div ref="cardsRef" v-motion-slide-right
+        class="grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+        <div v-for="(items, index) in displayedCards" :key="index"
+          class="flex flex-col border border-transparent rounded-lg bg-#2c2c2c mx-2 my-5 hover:bg-#5f5f5f hover:scale-103% transition-transform duration-300"
+          @click="handleClick(index, items)" @mouseenter="assetsDisplay(index)" @mouseleave="assetsDisplay('')">
+          <div class="h-270px relative overflow-hidden">
+            <div :class="normalDisplay === index ? 'translate-y-0' : 'translate-y-full'"
+              class="h-full absolute top-0 left-0 w-full transition-all duration-300">
+              <div class="h-95% my-1 overflow-y-scroll ">
+                <div v-for="e in items" :key="e" class="flex flex-row justify-between mx-2 text-3">
+                  <div>
+                    {{ e.asset_tag }}
+                  </div>
+                  <div>
+                    {{ e.asset_condition ? e.asset_condition : "Working" }}
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div :class="normalDisplay !== index ? 'translate-y-0' : '-translate-y-full'"
+              class="flex flex-col justify-evenly h-full pb-2 absolute top-0 left-0 w-full transition-all duration-300">
+              <div class="border border-gray-7 rounded-lg h-70% mb-1 flex justify-center ">
+                <img :src="getImageUrl(items) ? getImageUrl(items) : imagePlaceHolder" alt="Image"
+                  class="max-w-100% max-h-100% object-contain rounded-lg">
+              </div>
+
+              <div class="mx-2 text-justify">
+                <div class="truncate font-bold text-5">
+                  {{ index }}
+                </div>
+                <div class="truncate text-3.5 text-gray-4">
+                  Qty: {{ items.length }}
+                </div>
+                <div class="truncate text-3.5">
+                  <AChip :color="category[items[0].category].color">
+                    {{ items[0].category }}
+                  </AChip>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+
+  </div>
 </template>
 
 <style scoped>
 /* Scrollbar styles */
 ::-webkit-scrollbar {
-    width: 8px;
+  width: 8px;
 }
 
 ::-webkit-scrollbar-track {
-    background: #e2e2e2;
-    border-radius: 8px;
+  background: #e2e2e2;
+  border-radius: 8px;
 }
 
 ::-webkit-scrollbar-thumb {
-    background: #9A6BED;
-    border-radius: 8px;
+  background: #9A6BED;
+  border-radius: 8px;
 }
 
 ::-webkit-scrollbar-thumb:hover {
-    background: #A489D4;
+  background: #A489D4;
 }
 </style>
